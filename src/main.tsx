fc76fc5e4011bf83011502dacca77a2a1df26337
ruby <rehab.khaled.bahnasy@gmail.com>
2024-08-19 21:41:49 +0300

    Added the icons on the top right.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import IconButtons from './components/IconButton'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <IconButtons></IconButtons>
   </StrictMode>,
 )
