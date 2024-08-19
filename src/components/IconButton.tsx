fae90903a098dde46e9d647a672fc6c892742a51
MoBakry04 <mowaelbk@gmail.com>
2024-08-20 00:13:21 +0300

    Added Github And Video Buttons.

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import './IconButtonStyle.css'
-import gitHubIcon from 'F:/Algolizer/GS_Visualizer/src/assets/githubIcon.png'
-import videoIcon from 'F:/Algolizer/GS_Visualizer/src/assets/videoIcon.png'
+import React from "react";
+import "./IconButtonStyle.css";
+import gitHubIcon from "C:/Users/Mohamed Bakry/OneDrive/Desktop/Algolizer/GS_Visualizer/src/assets/githubIcon.png";
+import videoIcon from "C:/Users/Mohamed Bakry/OneDrive/Desktop/Algolizer/GS_Visualizer/src/assets/videoIcon.png";
 function IconButtons() {
   const handleVideoClick = () => {
-    console.log('Video button clicked');
+    console.log("Video button clicked");
   };
 
   const handleGitHubClick = () => {
-    console.log('GitHub button clicked');
+    console.log("GitHub button clicked");
   };
   return (
-    <div className='iconsDiv'>
-      <button className='video ' onClick = {handleVideoClick}> 
-        <img className='png' src = {videoIcon}></img>  </button>
-      <button className='git' onClick = {handleGitHubClick}><img className='png' src={gitHubIcon}></img></button>
+    <div className="iconsDiv">
+      <div className="videodiv">
+        <button className="video " onClick={handleVideoClick}>
+          <img className="png" src={videoIcon}></img>{" "}
+        </button>
+      </div>
+      <div className="gitdiv">
+        <button className="git" onClick={handleGitHubClick}>
+          <img className="png" src={gitHubIcon}></img>
+        </button>
+      </div>
+      
     </div>
   );
 }
-
 export default IconButtons;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import IconButtons from './components/IconButton'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
