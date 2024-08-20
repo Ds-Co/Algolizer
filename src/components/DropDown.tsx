75f996dc2ea70d935e70833046fef5a3ce515715
Akram1208 <mohamed.akram1208@gmail.com>
2024-08-20 13:05:37 +0300

    Fixed The Dropdown Menu.

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
-
-function Dropdown({ sorts }: { sorts: string[] }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
+const DropDown = ({sorts} : {sorts:string[]}) => {
   return (
-    <div className="dropdown">
-      <button
-        onClick={toggleDropdown}
-        className="btn btn-secondary dropdown-toggle"
-        type="button"
-        data-bs-toggle="dropdown"
-        data-bs-auto-close="true"
-        aria-expanded={"false"}
-      >
-        Sorting Algorithms{" "}
-      </button>
-      {isOpen && (
-        <ul className="dropdown-content">
-          {sorts.map((sort, index) => (
-            <li>
-              <a key={index} href="#">
-                {sort}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+    <select className="sort-dropdown">
+      {sorts.map((SortOption,index) => (<option key={index}>{SortOption}</option>))}
+      </select>
+    </>
   );
-}
+};
 
-export default Dropdown;
+export default DropDown;
