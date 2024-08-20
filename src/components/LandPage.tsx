2b6bd85afa805d87731e504eac495b7089f64f94
ruby <rehab.khaled.bahnasy@gmail.com>
2024-08-20 04:58:29 +0300

    Added Logo and Text.

---
+++ src/components/LandPage.tsx
@@ -0,0 +1,16 @@
+import IconButtons from './IconButton';
+import LogoIcon from './LogoIcon';
+import './LandPageStyle.css'
+function LandPage() {
+  return (
+    <div className="container">
+    <div>
+      <IconButtons/>
+      </div>
+    <div>
+      <LogoIcon/>
+      </div>
+    </div>
+  );
+}
+export default LandPage;
