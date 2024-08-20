5f42118ac88d350d00f61c9b3a7b6a62b75a8191
ruby <rehab.khaled.bahnasy@gmail.com>
2024-08-20 06:20:11 +0300

    Added the LandPage text.

--- src/components/LandPage.tsx
+++ src/components/LandPage.tsx
@@ -1,14 +1,20 @@
 import IconButtons from './IconButton';
 import LogoIcon from './LogoIcon';
+import TextLandPage from './textLandPage';
 import './LandPageStyle.css'
 function LandPage() {
   return (
     <div className="container">
     <div>
       <IconButtons/>
+    </div>
+    <div className='bulk'>
+      <div>
+        <LogoIcon/>
       </div>
-    <div>
-      <LogoIcon/>
+      <div>
+        <TextLandPage/>
+      </div>
       </div>
     </div>
   );
