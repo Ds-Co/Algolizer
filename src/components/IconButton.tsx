7c8291d391318d21047345d09c1b6e6bc0a53b50
Parlerler <malekwa3er99@gmail.com>
2024-08-24 16:25:55 +0300

    adjusted overflow and imports

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,48 +1,54 @@
-import  {useState} from "react";
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
 import "./IconButtonStyle.css";
-import gitHubIcon from 'F:/Algolizer/GS_Visualizer/src/assets/githubIcon.png';
-import videoIcon from 'F:/Algolizer/GS_Visualizer/src/assets/videoIcon.png';
-import arrowIcon from 'F:/Algolizer/GS_Visualizer/src/assets/arrowIcon.png';
-const videoLink  = "https://docs.google.com/document/d/1wSe3c5Pk82lZJgtE1njiQ98LDyY3Dlw9zReZuX7tExw/edit?usp=sharing";
+import gitHubIcon from "../assets/githubIcon.png";
+import videoIcon from "../assets/videoIcon.png";
+import arrowIcon from "../assets/arrowIcon.png";
+const videoLink =
+  "https://docs.google.com/document/d/1wSe3c5Pk82lZJgtE1njiQ98LDyY3Dlw9zReZuX7tExw/edit?usp=sharing";
 const gitLink = "https://github.com/Ds-Co/GS_Visualizer";
 
-interface Icon{
-  icon : string;
-  className : string;
-  url : string;
+interface Icon {
+  icon: string;
+  className: string;
+  url: string;
 }
-function IconButton({icon , className , url} : Icon){
-  const [Hovered , setHovered] = useState(false);
+function IconButton({ icon, className, url }: Icon) {
+  const [Hovered, setHovered] = useState(false);
   const HoverEnter = () => setHovered(true);
   const HoverLeave = () => setHovered(false);
-  const Click = () => window.open(url, '_blank');
+  const Click = () => window.open(url, "_blank");
   return (
-    <img className = {`${className} ${Hovered ? 'hovered' : ''}`} onMouseEnter={HoverEnter} onMouseLeave={HoverLeave} src = {icon} onClick={Click}></img>
+    <img
+      className={`${className} ${Hovered ? "hovered" : ""}`}
+      onMouseEnter={HoverEnter}
+      onMouseLeave={HoverLeave}
+      src={icon}
+      onClick={Click}
+    ></img>
   );
-
 }
 function IconButtons() {
-  
-
   return (
-    <div className = "icons">
-      <div className = "iconsDiv">
-        <div className = "videodiv">
-          <button className = "video"> 
-           <IconButton icon = {videoIcon} className="png" url = {videoLink}/>
+    <div className="icons">
+      <div className="iconsDiv">
+        <div className="videodiv">
+          <button className="video">
+            <IconButton icon={videoIcon} className="png" url={videoLink} />
           </button>
         </div>
-        <div className = "gitdiv">
-          <button className = "git">
-            <IconButton icon = {gitHubIcon} className="png" url = {gitLink} />
+        <div className="gitdiv">
+          <button className="git">
+            <IconButton icon={gitHubIcon} className="png" url={gitLink} />
           </button>
         </div>
       </div>
-      <div className = "arrowDiv">
-        <button className = "arrow">
-        <IconButton icon = {arrowIcon} className="arrowPng" url = "https://github.com/Ds-Co/GS_Visualizer"/>
+      <div className="arrowDiv">
+        <button className="arrow">
+          <IconButton
+            icon={arrowIcon}
+            className="arrowPng"
+            url="https://github.com/Ds-Co/GS_Visualizer"
+          />
         </button>
       </div>
     </div>
