25bd80bfd7d86ba93bb4bb40121f90ed1e448aa7
ruby <rehab.khaled.bahnasy@gmail.com>
2024-08-24 17:03:32 +0300

    Refactored the code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LandPage from './components/LandPage'
-import ParticleSystem from './components/BackGroundAnimation'
+import LandPage from './components/LandPage/LandPage'
+import ParticleSystem from './components/LandPage/BackGroundAnimation'
 import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
