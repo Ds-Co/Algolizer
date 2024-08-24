a888e400dab63df53b715901f351401291a69650
ruby <rehab.khaled.bahnasy@gmail.com>
2024-08-24 18:37:57 +0300

    Finished LandPage.
    Co-authored-by: Parlerler <Parlerler@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandPage from './components/LandPage/LandPage'
-import ParticleSystem from './components/LandPage/BackGroundAnimation'
+import SplitScreen from './components/SplitScreen/SplitScreen'
 import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <>
-    <LandPage/>
-    <ParticleSystem/>
-    
-    </>
-  
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandPage/>}></Route>
+        <Route path='/SplitScreen' element={<SplitScreen/>}></Route>
+      </Routes>
+    </Router>
+  </StrictMode>
 )
